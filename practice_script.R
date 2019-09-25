59ca7d5442b7d01dc256bd3ccc19b672af8c9d19
Figueroa <fig009@csiro.au>
2019-09-25 10:41:44 +1000

    some notes

---
+++ practice_script.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+#how to post scripts in slack
+
+#`some text` press enter (back tick, content,backtick, enter)
+
+#if you have a longer bit, you can multiple lines within ``` then use shift and enter gives you a new line, 
+#shift enter again and close off code with ``` press enter
+
+#final way, when you have more coed like 10-11 lines, 
+#use paper click, create new, creat snippet, put code in content, remember to enter the type of code
+
